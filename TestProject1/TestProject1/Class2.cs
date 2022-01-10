f796ff383d3f9d128e93dbd00ecc06813019b72e
Viktoriya2 Antonova <v2.antonova@a1qa.com>
2022-01-10 11:40:44 +0300

    first project

---
+++ TestProject1/TestProject1/Class2.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Allure.Core;
+using NUnit.Allure.Steps;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    [AllureNUnit]
+    class Class2
+    {
+        [AllureStep]
+        public void MethodExample()
+        {
+            //Step name = "MethodExample"
+        }
+
+        [Test]
+        public void Test1()
+        {
+            StepSample("user", "pwd123");
+        }
+
+        [AllureStep("Login with user {0} and password {1}")]
+        public void StepSample(string username, string password)
+        { }
+    }
+}
---
+++ TestProject1/TestProject1/ClassLifecycle.cs
@@ -0,0 +1,50 @@
+﻿using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Allure.Steps;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject1
+{
+    [TestFixture(Author = "unickq", Description = "Examples")]
+    [AllureNUnit]
+    [AllureLink("https://github.com/unickq/allure-nunit")]
+    class ClassLifecycle
+    {
+        //[OneTimeSetUp]
+        //public void ClearResultsDir()
+        //{
+        //    AllureLifecycle.Instance.CleanupResultDirectory();
+        //}
+
+        ////Allure.Steps required
+        //[AllureStep("This method is just saying hello")]
+        //private void SayHello()
+        //{
+        //    Console.WriteLine("Hello!");
+        //}
+
+        //[Test]
+        //[AllureTag("NUnit", "Debug")]
+        //[AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")]
+        //[AllureSeverity(SeverityLevel.critical)]
+        //[AllureFeature("Core")]
+        //public void EvenTest([Range(0, 5)] int value)
+        //{
+        //    SayHello();
+
+        //    //Wrapping Step
+        //    AllureLifecycle.Instance.WrapInStep(
+        //        () => { Assert.IsTrue(value % 2 == 0, $"Oh no :( {value} % 2 = {value % 2}"); },
+        //        "Validate calculations");
+        //}
+
+        //public static void MakeScreenshot(string screenshotName = "Скриншот")
+        //{
+        //    AllureLifecycle.AddAttachment("", "", "Attachment Title");
+        //}
+    }
+}
